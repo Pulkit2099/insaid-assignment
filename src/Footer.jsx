07cb53827868b66bf0a6143e1acdb8fd52e7d13c
Pulkit2099 <guptapulkit71@gmail.com>
2023-01-26 18:15:05 +0530

    insaid

---
+++ src/Footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+   <>
+   
+       <footer  className='bg-light text-center '>
+<p>
+    Get ready for BUSSINESS WITH INSAID LABS
+</p>
+       </footer>
+
+   
+   
+   </>
+  )
+}
+
+export default Footer
